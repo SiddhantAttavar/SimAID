e2029f255aca7c898d7f6e595a1fcecb6333491f
siddhantacounts@gmail.com <siddhantacounts@gmail.com>
2021-06-06 14:08:14 +0000

    Created Main; Edited settings; Deleted Form1

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -1,8 +1,7 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import MainTemplate
 from anvil import *
 
-class Form1(Form1Template):
-
+class Main(MainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
