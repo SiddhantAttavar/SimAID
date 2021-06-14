cd50b21e565335db475a2f9e87ae623241029cd0
siddhantacounts@gmail.com <siddhantacounts@gmail.com>
2021-06-14 10:19:45 +0000

    Edited Main, settings, RunSimulation and SimulationControls

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import MainTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 from ..RunSimulation import RunSimulation
 from ..SimulationControls import SimulationControls
@@ -14,9 +17,9 @@ class Main(MainTemplate):
   def onSimulationControlsLinkClick(self, **event_args):
     """This method is called when the simulation controls link in the navbar is clicked"""
     self.root.clear()
-    self.root.add_component(SimulationControls())
+    self.root.add_component(SimulationControls(), full_width_row = True)
 
   def onSimulationLinkClick(self, **event_args):
     """This method is called when the simulation link in the navbar is clicked"""
     self.root.clear()
-    self.root.add_component(RunSimulation())
+    self.root.add_component(RunSimulation(), full_width_row = True)
--- client_code/RunSimulation/__init__.py
+++ client_code/RunSimulation/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import RunSimulationTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import plotly.graph_objects as go
 
 class RunSimulation(RunSimulationTemplate):
