26c93b4702b96c5831e60cc70c1ed87c3dc29880
siddhantacounts@gmail.com <siddhantacounts@gmail.com>
2021-06-14 16:42:53 +0000

    Created Simulation.Frame

---
+++ client_code/Simulation/Frame.py
@@ -0,0 +1,14 @@
+from anvil import server
+
+# Contains the skeleton of the Frame Class
+
+@server.portable_class('Frame')
+class Frame:
+  
+  # Methods
+  def __init__(self, people):
+    pass
+
+@server.portable_class('Person')
+class Person():
+  pass
