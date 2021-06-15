950eaacbf896677f44ab5950ffb3c40083ce116e
siddhantacounts@gmail.com <siddhantacounts@gmail.com>
2021-06-15 18:46:23 +0000

    Edited UserInterface.RunSimulation

--- client_code/UserInterface/RunSimulation/__init__.py
+++ client_code/UserInterface/RunSimulation/__init__.py
@@ -73,8 +73,7 @@ class RunSimulation(RunSimulationTemplate):
     # Draw each person on the canvas as a dot
     for person in frame.people:
       self.canvas.arc(person.x * self.width, person.y * self.height, 5)
-      self.canvas.fill_style = 'blue'
-      print(person.state.color)
+      self.canvas.fill_style = person.state.color
       self.canvas.fill()
 
   def onRunSimulationButtonClick(self, **event_args):
