eb6d21b9707b905744aaf572f4538a2c35ae1998
siddhantacounts@gmail.com <siddhantacounts@gmail.com>
2021-06-17 13:53:16 +0000

    Fixed indentation and added documentation

--- client_code/UserInterface/Main/__init__.py
+++ client_code/UserInterface/Main/__init__.py
@@ -15,8 +15,7 @@ class Main(MainTemplate):
 
 		# Any code you write here will run when the form opens.
 		self.simulationControlsForm = SimulationControls()
-		self.root.add_component(self.simulationControlsForm, full_width_row = True)
-		
+		self.root.add_component(self.simulationControlsForm, full_width_row = True)		
 
 	def onSimulationControlsLinkClick(self, **event_args):
 		'''This method is called when the simulation controls link in the navbar is clicked'''
