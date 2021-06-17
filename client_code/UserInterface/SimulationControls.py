a6bf7484dad6f93a848a1b404b407a13970b12ea
siddhantacounts@gmail.com <siddhantacounts@gmail.com>
2021-06-17 10:31:21 +0000

    Edited UserInterface.SimulationControls and CustomComponents.Slider

--- client_code/UserInterface/SimulationControls.py
+++ client_code/UserInterface/SimulationControls.py
@@ -14,10 +14,30 @@ class SimulationControls(SimulationControlsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    self.populationSizeSlider.default = Params.POPULATION_SIZE
-    self.populationSizeLabel.text = f'Population Size (10 - 1000): {Params.POPULATION_SIZE}'
+    # Initalize all the options based on the default values of Params
+    self.params = Params()
     
-
-  def onPopulationSizeSliderChange(self, **event_args):
+    self.populationSizeSlider.default = self.params.POPULATION_SIZE
+    self.populationSizeLabel.text = f'Population Size (10 - 1000): {self.populationSizeSlider.default}'
+    
+    self.simulationLengthSlider.default = self.params.SIMULATION_LENGTH
+    self.simulationLengthLabel.text = f'Simulation Length (10 - 500): {self.simulationLengthSlider.default}'
+    
+    self.infectionRateSlider.default = int(self.params.INFECTION_RATE * 100)
+    self.infectionRateLabel.text = f'Infection Rate (1 - 100): {self.infectionRateSlider.default}'
+    
+  def onPopulationSizeChange(self, **event_args):
     """This method is called when the population size slider is moved"""
+    self.params.POPULATION_SIZE = self.populationSizeSlider.level
     self.populationSizeLabel.text = f'Population Size (10 - 1000): {self.populationSizeSlider.level}'
+
+  def onSimulationLengthChange(self, **event_args):
+    """This method is called when the simulation length slider is moved"""
+    self.params.SIMULATION_LENGTH = self.simulationLengthSlider.level
+    self.simulationLengthLabel.text = f'Simulation Length (10 - 500): {self.simulationLengthSlider.level}'
+
+  def onInfectionRateChange(self, **event_args):
+    """This method is called when the infection rate slider is moved"""
+    self.params.INFECTION_RATE = self.infectionRateSlider.level / 100
+    self.infectionRateLabel.text = f'Infection Rate (1 - 100): {self.infectionRateSlider.level}'
+
