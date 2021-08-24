ef0cdcce5a88f3caaaaca44b301624157d6c2c08
siddhantacounts@gmail.com <siddhantacounts@gmail.com>
2021-08-24 16:22:48 +0000

    Created ServerModule1

---
+++ server_code/ServerModule1.py
@@ -0,0 +1,12 @@
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+
+@anvil.server.callable
+def getSimulations(user):
+  return app_tables.simulations.search(user = user)
